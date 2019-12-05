5164389ab345ac081a935f3ea440223eb7c7b7e7
Elizabeth Van Hook and Matthew Swart <matthew@>
2019-12-04 20:45:16 -0800

    added styles

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,6 @@
 
-  var add = function(number1, number2) {
-    return number1 + number2;
+  var add = function(number1, number2, number3) {
+    return number1 + number2 + number3;
   };
 
   var subtract = function(number1, number2) {
@@ -43,16 +43,18 @@ $(document).ready(function() {
     event.preventDefault();
     var question1 = parseInt($("input:radio[name=question1]:checked").val());
     var question2 = parseInt($("input:radio[name=question2]:checked").val());
-    var result = add(question1, question2);
+    var question3 = parseInt($("input:radio[name=question3]:checked").val());
+    var result = add(question1, question2, question3);
     console.log(question1);
     console.log(question2);
+    console.log(question3);
     console.log(result);
     var display;
-    if (result > "9"){
+    if (result > "12"){
       display = "You are Hermione Granger";
-    } else if (result > "7") {
+    } else if (result >= "7" && result <= "11") {
       display = "You are Harry Potter";
-    } else if (result < "7") {
+    } else if (result < "6") {
       display = "You are Ron Weasley";
     }
     $("#whoYouAre").text(display);
